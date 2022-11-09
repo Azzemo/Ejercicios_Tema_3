0f4a99bd53d437f5bc85eb866fddfd9b409e4b1f
Azzemod <117802691+Azzemo@users.noreply.github.com>
2022-11-09 07:29:21 -0400

    Add files via upload

---
+++ Parte1.java
@@ -0,0 +1,13 @@
+ public class Parte1 {
+
+    public static void main(String[] args) {
+        int resultado = suma(20, 30, 180);
+        System.out.println("el resultado es " + resultado);
+    }
+    
+    public static int suma(int a, int b, int c) {
+        int resultado = a + b + c;
+        return resultado;
+        
+       }
+}
---
+++ Parte2.java
@@ -0,0 +1,23 @@
+
+public class Parte2 {
+
+    public static void main(String[] args) {
+        Coche micoche = new Coche();
+        micoche.Añadir();
+        System.out.println(micoche.puertas);
+        System.out.println(micoche.puertas);
+        
+         
+    }
+}
+
+class Coche {
+    public int puertas   = 2;
+    public void Añadir() {
+    this.puertas++;
+   }
+        
+    }
+  
+
+
